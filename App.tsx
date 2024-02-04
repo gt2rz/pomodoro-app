f6920364975b71d1d1e5c57c8e214b55a99de946
Miguel Gutierrez <gt2rz.dev@gmail.com>
2024-02-04 17:49:13 -0500

    Add start/pause and stop functionality to Pomodoro timer

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useState} from 'react';
 import {
   SafeAreaView,
@@ -19,22 +19,65 @@ function App(): React.JSX.Element {
   const [time, setTime] = useState(25 * 60);
   const [status, setStatus] = useState(options[0].id);
 
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTime(prevTime => {
+          if (prevTime === 0) {
+            clearInterval(interval!);
+            setIsRunning(false);
+            setTime(options[status - 1].time * 60);
+            return 0;
+          }
+
+          return prevTime - 1;
+        });
+      }, 10);
+    } else {
+      if (interval) {
+        clearInterval(interval!);
+      }
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [isRunning, status]);
+
+  const handleStartAndPause = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+    setTime(options[status - 1].time * 60);
+  };
+
   return (
     <SafeAreaView
       style={[styles.container, {backgroundColor: options[status - 1].color}]}>
-      <StatusBar />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <Text style={styles.text}>Pomodoro</Text>
           <Header status={status} setStatus={setStatus} setTime={setTime} />
           <Timer time={time} />
           <TouchableOpacity
-            onPress={() => setIsRunning(!isRunning)}
+            onPress={() => handleStartAndPause()}
             style={styles.button}>
             <Text style={[styles.textWhite, styles.textCenter]}>
-              {isRunning ? 'STOP' : 'START'}
+              {isRunning ? 'PAUSE' : 'START'}
             </Text>
           </TouchableOpacity>
+          {isRunning && (
+            <TouchableOpacity
+              onPress={() => handleStop()}
+              style={[styles.button, styles.buttonStop]}>
+              <Text style={[styles.textWhite, styles.textCenter]}>STOP</Text>
+            </TouchableOpacity>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -44,6 +87,7 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: StatusBar.currentHeight,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -66,6 +110,9 @@ const styles = StyleSheet.create({
   },
   textCenter: {
     textAlign: 'center',
+  },
+  buttonStop: {
+    backgroundColor: 'red',
   },
 });
 
